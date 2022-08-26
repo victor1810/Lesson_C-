62aed4d4cea44f4f300d8fbc0b5884115dd7ee78
victor1810 <victor181078@gmail.com>
2022-08-26 20:06:09 +0300

    add Lesson C# 2

--- Lesson_C#_1/Program.cs
+++ Lesson_C#_1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = 0;
+int b = 0;
+Console.Write("Введите числои степень: ");
+
+if (int.TryParse(Console.ReadLine(), out number) && int.TryParse(Console.ReadLine(), out b))
+    Console.Write($"sqr of {number, b});
+
+Console.Write($"numbers = {numbers * numbers}");
